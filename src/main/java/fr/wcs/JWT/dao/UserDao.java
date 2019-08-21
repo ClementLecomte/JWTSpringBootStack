a14b0570f8ef72344f876338c3965b1d19091066
ClementLecomte <lecomte.c49@gmail.com>
2019-08-21 22:08:26 +0100

    Stack de connexion en API avec le JWT Token + mysql

---
+++ src/main/java/fr/wcs/JWT/dao/UserDao.java
@@ -0,0 +1,10 @@
+package fr.wcs.JWT.dao;
+
+import fr.wcs.JWT.models.DAOUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+    DAOUser findByUsername(String username);
+}
